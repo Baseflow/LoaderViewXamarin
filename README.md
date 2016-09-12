c71fc7b5d15ca9ba225c629011a4ab61b07c5f4a
Martijn van Dijk <mhvdijk@gmail.com>
2016-09-12 16:02:06 +1000

    Initial version of LoaderView

---
+++ README.md
@@ -0,0 +1,11 @@
+# LoaderViewXamarin
+Library that enables TextView of ImageView to show loading animation while waiting for the text and image get loaded
+
+Xamarin bindings library for loaderviewlibrary [library](https://github.com/elye/loaderviewlibrary).
+
+Plugin is available on [Nuget](https://www.nuget.org/packages/Xam.Plugins.Android.LoaderView/).
+
+#### Preview
+![Timer Image](https://static.wixstatic.com/media/d748c3_28381c0f110f4dc68fcd340b503f86a2~mv2.gif)
+
+See the sample for implementation.
---
+++ Sample/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    [Activity(Label = "LoaderView", MainLauncher = true, Icon = "@mipmap/icon", Theme="@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            loadData();
+        }
+
+        private void loadData()
+        {
+            Task.Delay(3000).ContinueWith((arg) => postLoadData());
+        }
+
+        private void postLoadData()
+        {
+            RunOnUiThread(() =>
+            {
+                FindViewById<TextView>(Resource.Id.txt_name).Text = "Mr. Donald Trump";
+                FindViewById<TextView>(Resource.Id.txt_title).Text = "Presidency Candidate of United State";
+                FindViewById<TextView>(Resource.Id.txt_phone).Text = "+001 2345 6789";
+                FindViewById<TextView>(Resource.Id.txt_email).Text = "donald.trump@donaldtrump.com";
+                FindViewById<ImageView>(Resource.Id.image_icon).SetImageResource(Resource.Drawable.trump);
+            });
+        }
+    }
+}
+
+
